ace4ed6aa1f269e249b84ce19a615e18b68c3cd3
Tom De Vylder <tom@penumbra.be>
2012-11-26 10:53:48 +0100

    Add service hassrestart support. Ensure it to default to false to make sure the service picks up new configuration settings.

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,39 +4,41 @@ class keepalived::params {
 
   case $::operatingsystem {
     'RedHat', 'CentOS': {
-      $config_dir        = ''
-      $config_group      = 'root'
-      $config_dir_mode   = '0755'
-      $config_mode       = '0644'
-      $config_user       = 'root'
-      $daemon_group      = 'root'
-      $daemon_user       = 'root'
-      $log_dir           = ''
-      $pid_file          = ''
-      $pkg_ensure        = present
-      $pkg_list          = ''
-      $service_enable    = true
-      $service_ensure    = running
-      $service_hasstatus = ''
-      $service_name      = ''
+      $config_dir         = ''
+      $config_group       = 'root'
+      $config_dir_mode    = '0755'
+      $config_mode        = '0644'
+      $config_user        = 'root'
+      $daemon_group       = 'root'
+      $daemon_user        = 'root'
+      $log_dir            = ''
+      $pid_file           = ''
+      $pkg_ensure         = present
+      $pkg_list           = ''
+      $service_enable     = true
+      $service_ensure     = running
+      $service_hasstatus  = ''
+      $service_hasrestart = false
+      $service_name       = ''
     }
 
     'Debian', 'Ubuntu': {
-      $config_dir        = '/etc/keepalived'
-      $config_dir_mode   = '0755'
-      $config_file_mode  = '0644'
-      $config_group      = 'root'
-      $config_user       = 'root'
-      $daemon_group      = 'root'
-      $daemon_user       = 'root'
-      $log_dir           = ''
-      $pid_file          = '/var/run/keepalived.pid'
-      $pkg_ensure        = present
-      $pkg_list          = [ 'keepalived' ]
-      $service_enable    = true
-      $service_ensure    = running
-      $service_hasstatus = false
-      $service_name      = 'keepalived'
+      $config_dir         = '/etc/keepalived'
+      $config_dir_mode    = '0755'
+      $config_file_mode   = '0644'
+      $config_group       = 'root'
+      $config_user        = 'root'
+      $daemon_group       = 'root'
+      $daemon_user        = 'root'
+      $log_dir            = ''
+      $pid_file           = '/var/run/keepalived.pid'
+      $pkg_ensure         = present
+      $pkg_list           = [ 'keepalived' ]
+      $service_enable     = true
+      $service_ensure     = running
+      $service_hasrestart = false
+      $service_hasstatus  = false
+      $service_name       = 'keepalived'
     }
 
     default: {
--- manifests/service.pp
+++ manifests/service.pp
@@ -2,10 +2,11 @@
 #
 class keepalived::service {
   service { $::keepalived::service_name:
-    ensure    => $::keepalived::service_ensure,
-    enable    => $::keepalived::service_enable,
-    hasstatus => $::keepalived::service_hasstatus,
-    require   => Class['::keepalived::config'];
+    ensure     => $::keepalived::service_ensure,
+    enable     => $::keepalived::service_enable,
+    hasrestart => $::keepalived::service_hasrestart,
+    hasstatus  => $::keepalived::service_hasstatus,
+    require    => Class['::keepalived::config'];
   }
 }
 
