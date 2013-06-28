f695ed74d6a2978428b2c83b1c8b048928b2e98d
Tom De Vylder <tom@penumbra.be>
2013-06-28 17:09:56 +0200

    Add script rspec tests

---
+++ spec/defines/keepalived_vrrp_script_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+describe 'keepalived::vrrp::script', :type => :define do
+  let (:facts) { debian_facts }
+  let (:pre_condition) { '$concat_basedir = "/tmp"' }
+
+  describe 'without parameters', :type => :define do
+    let (:facts) { debian_facts }
+    let (:title) { '_TITLE_' }
+
+    it do
+      expect { should create_class('keepalived') }.to raise_error(Puppet::Error)
+    end
+  end
+
+  describe 'with parameter interval' do
+    let (:title) { '_TITLE_' }
+    let (:params) {
+      {
+        :interval => '_VALUE_',
+        :script => '_SCRIPT_'
+      }
+    }
+
+    it { should create_keepalived__vrrp__script('_TITLE_') }
+    it {
+      should \
+        contain_concat__fragment('keepalived.conf_vrrp_script__TITLE_').with(
+        'content' => /interval.*_VALUE_/
+      )
+    }
+  end
+
+  describe 'with parameter script' do
+    let (:title) { '_TITLE_' }
+    let (:params) {
+      {
+        :script => '_VALUE_'
+      }
+    }
+
+    it { should create_keepalived__vrrp__script('_TITLE_') }
+    it {
+      should \
+        contain_concat__fragment('keepalived.conf_vrrp_script__TITLE_').with(
+        'content' => /script.*_VALUE_/
+      )
+    }
+  end
+
+  describe 'with parameter weight' do
+    let (:title) { '_TITLE_' }
+    let (:params) {
+      {
+        :weight => '_VALUE_',
+        :script => '_SCRIPT_'
+      }
+    }
+
+    it { should create_keepalived__vrrp__script('_TITLE_') }
+    it {
+      should \
+        contain_concat__fragment('keepalived.conf_vrrp_script__TITLE_').with(
+        'content' => /weight.*_VALUE_/
+      )
+    }
+  end
+end
+
