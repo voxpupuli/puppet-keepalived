db8f0572454b2e0d3516da7119a921d440352e15
Graham Bleach <graham@songkick.com>
2013-10-22 11:47:26 +0100

    Initial LVS support.
    Currently supports a subset of possible configurations, listed in the docs.

---
+++ manifests/lvs/virtual_server.pp
@@ -0,0 +1,115 @@
+# == Define: keepalived::vrrp::virtual_server 
+#
+# Configure a Linux Virtual Server with keepalived
+#
+# Work in progress, supports:
+#   - single IP/port virtual servers 
+#   - TCP_CHECK healthchecks
+#
+# === Parameters
+#
+# Refer to keepalived's documentation to understand the behaviour of these parameters
+#
+# [*ip_address*]
+#   Virtual server IP address.
+#
+# [*port*]
+#   Virtual sever IP port.
+#
+# [*lb_algo*]
+#   Must be one of rr, wrr, lc, wlc, lblc, sh, dh
+#   Default: not set.
+#
+# [*delay_loop*]
+#   Default: not set.
+#
+# [*lb_kind*]
+#   Must be one of NAT, TUN, DR.
+#   Default: NAT
+#
+# [*ha_suspend*]
+#   Boolean.
+#   Default: false => not set in config.
+# 
+# [*alpha*]
+#   Boolean.
+#   Default: false => not set in config.
+# 
+# [*omega*]
+#   Boolean.
+#   Default: false => not set in config.
+# 
+# [*quorum*]
+#   Integer. 
+#   Defaults to unset => does not appear in config.
+#
+# [*hysteresis*]
+#   Integer. 
+#   Defaults to unset => does not appear in config.
+#
+# [*tcp_check*]
+#   The TCP_CHECK to configure for real_servers.
+#   Should be a hash containing these keys:
+#     [*connect_timeout*]
+#   Default: unset => no TCP_CHECK configured.
+#
+# [*sorry_server*]
+#   The sorry_server to define
+#   A hash with these keys:
+#     [*ip_address*]
+#     [*port*]
+#
+# [*real_servers*]
+#   The real servers to balance to.
+#   A hash (single real server) or an array of hashes.
+#   Hash keys:
+#     [*ip_address*]
+#     [*port*]
+# 
+define keepalived::lvs::virtual_server (
+  $ip_address,
+  $port,
+  $lb_algo,
+  $delay_loop=undef,
+  $lb_kind='NAT',
+  $ha_suspend=false,
+  $persistence_timeout=undef,
+  $virtualhost=undef,
+  $alpha=false,
+  $omega=false,
+  $quorum=undef,
+  $hysteresis=undef,
+  $tcp_check=undef,
+  $sorry_server=undef,
+  $real_servers=undef
+) {
+
+  if ( ! is_ip_address($ip_address) ) {
+    fail("Invalid IP address")
+  }
+
+  validate_re($port, '^[0-9]{1,5}$', "Invalid port: ${port}")
+  validate_re($lb_algo, '^(rr|wrr|lc|wlc|lblc|sh|dh)$', "Invalid lb_algo: ${lb_algo}")
+  if $delay_loop { validate_re($delay_loop, '^[0-9]+$', "Invalid delay_loop: ${delay_loop}") }
+  validate_re($lb_kind, '^(NAT|DR|TUN)$', "Invalid lb_kind: ${lb_kind}") 
+  validate_bool($ha_suspend)
+  validate_bool($alpha)
+  validate_bool($omega)
+  if $quorum { validate_re($quorum, '^[0-9]+$', "Invalid quorum: ${quorum}") }
+  if $hysteresis { validate_re($hysteresis, '^[0-9]+$', "Invalid hysteresis ${hysteresis}") }
+
+  if $sorry_server {
+    if ( ! is_ip_address($sorry_server['ip_address']) ) {
+      fail("Invalid sorry server IP address: ${sorry_server['ip_address']}")
+    }
+    validate_re($sorry_server['port'], '^[0-9]{1,5}$', "Invalid sorry serverport: ${sorry_server['port']}")
+  }
+
+  concat::fragment { "keepalived.conf_lvs_virtual_server_${name}":
+    ensure  => $ensure,
+    target  => "${keepalived::config_dir}/keepalived.conf",
+    content => template('keepalived/lvs_virtual_server.erb'),
+    order   => 250,
+  }
+
+}
---
+++ spec/defines/keepalived_lvs_virtual_server_spec.rb
@@ -0,0 +1,221 @@
+
+require 'spec_helper'
+
+describe 'keepalived::lvs::virtual_server', :type => 'define' do
+  let(:title) { '_TITLE_' }
+  let(:facts) { debian_facts }
+  let(:pre_condition) { '$concat_basedir = "/tmp"' }
+
+  context 'without parameters' do
+    let (:title) { '_TITLE_' }
+
+    it do
+      expect { should create_class('keepalived') }.to raise_error(Puppet::Error)
+    end
+  end
+
+  context 'with bare minimum: ip address, lb_algo and port' do
+    let(:params) {
+      {
+        :ip_address   => '10.1.1.1',
+        :port         => '8080',
+        :lb_algo      => 'lc',
+      }
+    }
+
+    it { 
+      should contain_concat__fragment('keepalived.conf_lvs_virtual_server__TITLE_').with( {
+        'content' => /^group _TITLE_ \{\s+virtual_server 10.1.1.1 8080\s+lb_algo lc\s+lb_kind NAT\s+protocol TCP\s+\}/
+      })
+    }
+  end
+
+  context 'with optional parameters' do
+    let(:params) {
+      {
+        :ip_address          => '10.1.1.1',
+        :port                => '8080',
+        :lb_algo             => 'lc',
+        :lb_kind             => 'NAT',
+        :delay_loop          => 60,
+        :persistence_timeout => 5,
+        :ha_suspend          => true,
+        :virtualhost         => 'example.com',
+        :alpha               => true,
+        :omega               => true,
+        :quorum              => 5,
+        :hysteresis          => 9,
+        :sorry_server        => { 'ip_address' => '10.1.1.3',
+                                  'port'       => '999'}
+      }
+    }
+    it { 
+      should contain_concat__fragment('keepalived.conf_lvs_virtual_server__TITLE_').with( {
+        'content' => /delay_loop 60\s+lb_algo lc\s+lb_kind NAT\s+persistence_timeout 5\s+ha_suspend/,
+        'content' => /virtualhost example.com\s+alpha\s+omega\s+quorum 5\s+hysteresis 9/,
+        'content' => /\s+sorry_server 10.1.1.3 999\s+/
+      })
+    }
+  end
+
+  context 'with invalid IP address' do
+    let(:title) { '_TITLE_' }
+    let(:params) {
+      {
+        :ip_address          => 'rubbish',
+        :port                => '8080',
+        :lb_algo             => 'lc',
+      }
+    }
+
+    it do 
+      expect { should contain_concat__fragment() }.to raise_error(Puppet::Error, /Invalid IP/) 
+    end
+  end
+
+  context 'with invalid port' do
+    let(:title) { '_TITLE_' }
+    let(:params) {
+      {
+        :ip_address          => '10.1.1.1',
+        :port                => 'something',
+        :lb_algo             => 'lc',
+      }
+    }
+
+    it do 
+      expect { 
+        should contain_concat__fragment('keepalived.conf_lvs_virtual_server__TITLE_') 
+      }.to raise_error(Puppet::Error, /Invalid port/) 
+    end
+  end
+
+  context 'with invalid delay_loop' do
+    let(:title) { '_TITLE_' }
+    let(:params) {
+      {
+        :ip_address          => '10.1.1.1',
+        :port                => '80',
+        :lb_algo             => 'rr',
+        :delay_loop          => 'NaN',
+      }
+    }
+
+    it do 
+      expect { 
+        should contain_concat__fragment('keepalived.conf_lvs_virtual_server__TITLE_') 
+      }.to raise_error(Puppet::Error, /Invalid delay_loop/) 
+    end
+  end
+
+  context 'with invalid lb_algo' do
+    let(:title) { '_TITLE_' }
+    let(:params) {
+      {
+        :ip_address          => '10.1.1.1',
+        :port                => '80',
+        :lb_algo             => 'rubbish',
+      }
+    }
+
+    it do 
+      expect { 
+        should contain_concat__fragment('keepalived.conf_lvs_virtual_server__TITLE_') 
+      }.to raise_error(Puppet::Error, /Invalid lb_algo/) 
+    end
+  end
+
+  context 'with invalid lb_kind' do
+    let(:title) { '_TITLE_' }
+    let(:params) {
+      {
+        :ip_address          => '10.1.1.1',
+        :port                => '80',
+        :lb_algo             => 'rr',
+        :lb_kind             => 'BAT',
+      }
+    }
+
+    it do 
+      expect { 
+        should contain_concat__fragment('keepalived.conf_lvs_virtual_server__TITLE_') 
+      }.to raise_error(Puppet::Error, /Invalid lb_kind/) 
+    end
+  end
+
+  context 'with a single real_server' do
+    let(:params) {
+      {
+        :ip_address   => '10.1.1.1',
+        :port         => '8080',
+        :lb_algo      => 'lc',
+        :real_servers => [ { 'ip_address' => '10.1.1.2',
+                             'port'       => '8081'}]
+      }
+    }
+
+    it { 
+      should contain_concat__fragment('keepalived.conf_lvs_virtual_server__TITLE_').with( {
+        'content' => /real_server 10.1.1.2 8081 \{\s+\}/
+      })
+    }
+  end
+
+  context 'with a real_server with a TCP_CHECK' do
+    let(:params) {
+      {
+        :ip_address   => '10.1.1.1',
+        :port         => '8080',
+        :lb_algo      => 'lc',
+        :tcp_check    => { 'connect_timeout' => 5 },
+        :real_servers => [ { 'ip_address' => '10.1.1.2',
+                             'port'       => '8081'}]
+      }
+    }
+
+    it { 
+      should contain_concat__fragment('keepalived.conf_lvs_virtual_server__TITLE_').with( {
+        'content' => /real_server 10.1.1.2 8081 \{\s+TCP_CHECK \{\s+connect_port 8081\s+connect_timeout 5\s+\}\s+\}/
+      })
+    }
+  end
+
+  context 'with a real_server with a TCP_CHECK' do
+    let(:params) {
+      {
+        :ip_address   => '10.1.1.1',
+        :port         => '8080',
+        :lb_algo      => 'lc',
+        :tcp_check    => { 'connect_timeout' => 5 },
+        :real_servers => [ { 'ip_address' => '10.1.1.2',
+                             'port'       => '8081'}]
+      }
+    }
+
+    it { 
+      should contain_concat__fragment('keepalived.conf_lvs_virtual_server__TITLE_').with( {
+        'content' => /real_server 10.1.1.2 8081 \{\s+TCP_CHECK \{\s+connect_port 8081\s+connect_timeout 5\s+\}\s+\}/
+      })
+    }
+  end
+
+  context 'two real_servers' do
+    let(:params) {
+      {
+        :ip_address   => '10.1.1.1',
+        :port         => '8080',
+        :lb_algo      => 'lc',
+        :real_servers => [ { 'ip_address' => '10.1.1.2',
+                             'port'       => '8081'},
+                           { 'ip_address' => '10.1.1.3',
+                             'port'       => '8082'}]
+      }
+    }
+
+    it { 
+      should contain_concat__fragment('keepalived.conf_lvs_virtual_server__TITLE_').with( {
+        'content' => /real_server 10.1.1.2 8081\s+\{\s+\}\s+real_server 10.1.1.3 8082\s+/
+      })
+    }
+  end
+end
