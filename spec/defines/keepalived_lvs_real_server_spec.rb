f0df39be58d9b229792ec0a593758a25956273ae
Devin Christensen <quixoten@gmail.com>
2014-03-12 11:54:44 -0600

    Consistently order real_server options
    I also made the final output a bit more appealing by adding some space
    around blocks.

--- spec/defines/keepalived_lvs_real_server_spec.rb
+++ spec/defines/keepalived_lvs_real_server_spec.rb
@@ -25,39 +25,6 @@ describe 'keepalived::lvs::real_server', :type => 'define' do
     }
   end
 
-  context 'options' do
-    let(:params) {{
-      :virtual_server => 'virtual_server',
-      :ip_address     => '127.3.4.5',
-      :port           => '789',
-      :options        => {
-        'inhibit_on_failure' => true,
-        'SMTP_CHECK' => {
-          'connect_timeout' => 10,
-          'host' => {
-            'connect_ip' => '127.0.0.1'
-          }
-        }
-      }
-    }}
-
-    it {
-      should contain_concat__fragment('keepalived.conf_lvs_real_server_test').with({
-        'content' => <<-CONTENT.gsub(/ {10}/, '  ')
-          real_server 127.3.4.5 789 {
-            inhibit_on_failure
-            SMTP_CHECK {
-              connect_timeout 10
-              host {
-                connect_ip 127.0.0.1
-              }
-            }
-          }
-        CONTENT
-      })
-    }
-  end
-
   context 'with an invalid IP address' do
     let(:title) { 'test' }
     let(:params) {
@@ -88,6 +55,99 @@ describe 'keepalived::lvs::real_server', :type => 'define' do
         should contain_concat__fragment('keepalived.conf_lvs_real_server_test') 
       }.to raise_error(Puppet::Error, /Invalid port/) 
     end
+  end
+
+  context 'with single-line and block options' do
+    let(:params) {{
+      :virtual_server => 'virtual_server',
+      :ip_address     => '127.3.4.5',
+      :port           => '789',
+      :options        => {
+        'weight' => 1,
+        'notify_up' => "'notify-send \"good to go!\"'",
+        'inhibit_on_failure' => true,
+        'SMTP_CHECK' => {
+          'connect_timeout' => 10,
+          'host' => {
+            'connect_ip' => '127.0.0.1'
+          }
+        }
+      }
+    }}
+
+    it {
+      should contain_concat__fragment('keepalived.conf_lvs_real_server_test').with({
+        'content' => <<-CONTENT.gsub(/ {10}/, '  ')
+          real_server 127.3.4.5 789 {
+            inhibit_on_failure
+            notify_up 'notify-send "good to go!"'
+            weight 1
+
+            SMTP_CHECK {
+              connect_timeout 10
+
+              host {
+                connect_ip 127.0.0.1
+              }
+            }
+          }
+        CONTENT
+      })
+    }
+  end
+
+  context 'with only block options' do
+    let(:params) {{
+      :virtual_server => 'virtual_server',
+      :ip_address     => '127.3.4.5',
+      :port           => '789',
+      :options        => {
+        'SMTP_CHECK' => {
+          'host' => {
+            'connect_ip' => '127.0.0.1'
+          }
+        }
+      }
+    }}
+
+    it {
+      should contain_concat__fragment('keepalived.conf_lvs_real_server_test').with({
+        'content' => <<-CONTENT.gsub(/ {10}/, '  ')
+          real_server 127.3.4.5 789 {
+            SMTP_CHECK {
+              host {
+                connect_ip 127.0.0.1
+              }
+            }
+          }
+        CONTENT
+      })
+    }
+  end
+
+  context 'with only single-line options' do
+    let(:params) {{
+      :virtual_server => 'virtual_server',
+      :ip_address     => '127.3.4.5',
+      :port           => '789',
+      :options        => {
+        'weight' => 1,
+        'notify_up' => "'notify-send \"good to go!\"'",
+        'inhibit_on_failure' => true,
+      }
+    }}
+
+    it {
+      should contain_concat__fragment('keepalived.conf_lvs_real_server_test').with({
+        'content' => <<-CONTENT.gsub(/ {10}/, '  ')
+          real_server 127.3.4.5 789 {
+            inhibit_on_failure
+            notify_up 'notify-send "good to go!"'
+            weight 1
+          }
+        CONTENT
+      })
+    }
   end
 
 end
