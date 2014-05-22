851e5befd3a513517af830fb8982e8b77b494e94
Steffen Zieger <me@saz.sh>
2014-05-22 16:32:10 +0200

    add support for Gentoo Linux

--- manifests/params.pp
+++ manifests/params.pp
@@ -36,6 +36,20 @@ class keepalived::params {
       $service_name       = 'keepalived'
     }
 
+    'gentoo': {
+      $config_dir         = '/etc/keepalived'
+      $config_dir_mode    = '0755'
+      $config_file_mode   = '0644'
+      $config_group       = 'root'
+      $config_owner       = 'root'
+      $daemon_group       = 'root'
+      $daemon_user        = 'root'
+      $pkg_list           = [ 'keepalived' ]
+      $service_hasrestart = false
+      $service_hasstatus  = false
+      $service_name       = 'keepalived'
+    }
+
     default: {
       fail "Operating system ${::operatingsystem} is not supported."
     }
