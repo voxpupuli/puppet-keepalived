b738c13570ca6b6c58274b00db153e5d74aed34f
Peter Souter <peter.souter@puppetlabs.com>
2015-01-16 17:33:46 -0800

    Adds `keepalived_version` fact to module
    Also adds a fact unit test
    
    Useful for detecting if you can add version specific changes such as unicast support. This could even be extended in the future to warn when keepalived version < 1.2.8

---
+++ lib/facter/keepalived_version.rb
@@ -0,0 +1,20 @@
+# Fact: keepalived_version
+#
+# Purpose: get keepalived version
+#
+# Resolution:
+#   Tests for presence of keepalived
+#   "keepalived --version" and matches on the tag after 'Keepalived v'
+#
+# Caveats:
+#   none
+#
+# Notes:
+#   None
+if Facter::Util::Resolution.which('keepalived')
+  Facter.add(:keepalived_version) do
+    setcode do
+      Facter::Util::Resolution.exec('keepalived --version 2>&1').lines.first.match(/Keepalived v(\d\.\d\.\d) .+/)[1]
+    end
+  end
+end
---
+++ spec/unit/facter/keepalived_version_spec.rb
@@ -0,0 +1,32 @@
+require "spec_helper"
+
+describe Facter::Util::Fact do
+  before {
+    Facter.clear
+    allow(Facter::Util::Resolution).to receive(:exec).with(anything())
+  }
+
+  describe "keepalived_version" do
+    context 'returns keepalived version when keepalived present' do
+      it do
+        keepalived_version_output = <<-EOS
+Keepalived v1.2.2 (10/03,2013)
+        EOS
+        allow(Facter::Util::Resolution).to receive(:which).with("keepalived").
+          and_return(true)
+        allow(Facter::Util::Resolution).to receive(:exec).with("keepalived --version 2>&1").
+          and_return(keepalived_version_output)
+        Facter.fact(:keepalived_version).value.should == "1.2.2"
+      end
+    end
+
+    context 'returns nil when keepalived not present' do
+      it do
+        allow(Facter::Util::Resolution).to receive(:exec)
+        allow(Facter::Util::Resolution).to receive(:which).with("keepalived").
+          and_return(false)
+        Facter.fact(:keepalived_version).should be_nil
+      end
+    end
+  end
+end
