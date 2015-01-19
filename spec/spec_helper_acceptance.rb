c75793a22bf70427b32500c5beee5315c11662a0
Peter Souter <peter.souter@puppetlabs.com>
2015-01-19 11:25:45 +0000

    Adds spec_helper_acceptance for beaker-rspec

---
+++ spec/spec_helper_acceptance.rb
@@ -0,0 +1,35 @@
+require 'beaker-rspec'
+
+# Install Puppet
+unless ENV['RS_PROVISION'] == 'no'
+  # This will install the latest available package on el and deb based
+  # systems fail on windows and osx, and install via gem on other *nixes
+  foss_opts = { :default_action => 'gem_install' }
+
+  if default.is_pe?; then install_pe; else install_puppet( foss_opts ); end
+
+  hosts.each do |host|
+    on host, "mkdir -p #{host['distmoduledir']}"
+  end
+end
+
+UNSUPPORTED_PLATFORMS = ['Suse','windows','AIX','Solaris']
+
+RSpec.configure do |c|
+  # Project root
+  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+  # Readable test descriptions
+  c.formatter = :documentation
+
+  # Configure all nodes in nodeset
+  c.before :suite do
+    # Install module and dependencies
+    hosts.each do |host|
+      copy_module_to(host, :source => proj_root, :module_name => 'keepalived')
+      shell("/bin/touch #{default['puppetpath']}/hiera.yaml")
+      on host, puppet('module install puppetlabs-stdlib --version >= 4.1.0'), { :acceptable_exit_codes => [0] }
+      on host, puppet('module install puppetlabs-concat --version >= 1.0.2'), { :acceptable_exit_codes => [0] }
+    end
+  end
+end
