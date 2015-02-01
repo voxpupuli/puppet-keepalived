9d4b2a7e4cd6e29d0ce26e888fb85129b6e41e19
Peter Souter <peter.souter@puppetlabs.com>
2015-02-01 13:34:35 +0000

    Adds test to check for error in #61
    TDD FTW, test written first! :smile:

--- spec/unit/facter/keepalived_version_spec.rb
+++ spec/unit/facter/keepalived_version_spec.rb
@@ -20,6 +20,19 @@ Keepalived v1.2.2 (10/03,2013)
       end
     end
 
+    context 'returns keepalived version when keepalived present and last number more than one digit' do
+      it do
+        keepalived_version_output = <<-EOS
+Keepalived v1.2.13 (08/07,2014)
+        EOS
+        allow(Facter::Util::Resolution).to receive(:which).with("keepalived").
+          and_return(true)
+        allow(Facter::Util::Resolution).to receive(:exec).with("keepalived --version 2>&1").
+          and_return(keepalived_version_output)
+        Facter.fact(:keepalived_version).value.should == "1.2.13"
+      end
+    end
+
     context 'returns nil when keepalived not present' do
       it do
         allow(Facter::Util::Resolution).to receive(:exec)
