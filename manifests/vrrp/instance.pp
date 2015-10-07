46984878fbbbd38210f1c072eedbef6cf76c4e6c
Erik Anderson <erik.anderson@mx.com>
2015-10-07 10:20:37 -0600

    Split integer and value check into seperate if statements

--- manifests/vrrp/instance.pp
+++ manifests/vrrp/instance.pp
@@ -116,10 +116,10 @@
 # $unicast_source_ip::     default IP for binding vrrpd is the primary IP
 #                          on interface. If you want to hide the location of vrrpd,
 #                          use this IP as src_addr for unicast vrrp packets.
-#                          Default: undef. 
+#                          Default: undef.
 #
 # $unicast_peers::         Do not send VRRP adverts over VRRP multicast group.
-#                          Instead send adverts to the list of ip addresses using 
+#                          Instead send adverts to the list of ip addresses using
 #                          a unicast design fashion.
 #
 #                          May be specified as an array with ip addresses
@@ -168,10 +168,16 @@ define keepalived::vrrp::instance (
 ) {
   $_name = regsubst($name, '[:\/\n]', '')
 
-  if (!is_integer($priority) or $priority < 1 or $priority > 254) {
+  if (!is_integer($priority)){
+    fail('priority must be an integer')
+  }
+  if ($priority < 1 or $priority > 254) {
     fail('priority must be an integer 1 >= and <= 254')
   }
-  if (!is_integer($virtual_router_id) or $virtual_router_id < 1 or $virtual_router_id > 255) {
+  if (!is_integer($virtual_router_id) {
+    fail('virtual_router_id must be an integer')
+  }
+  if ($virtual_router_id < 1 or $virtual_router_id > 255) {
     fail('virtual_router_id must be an integer >= 1 and <= 255')
   }
 
@@ -182,4 +188,3 @@ define keepalived::vrrp::instance (
     order   => '100',
   }
 }
-
