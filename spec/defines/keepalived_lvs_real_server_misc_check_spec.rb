ce264d06d09a8b8e85238e8d60048a826c35eb14
Arnd Hannemann <arnd.hannemann@credativ.de>
2016-01-22 17:28:01 +0100

    add define keepalived::lvs::real_server::misc_check

---
+++ spec/defines/keepalived_lvs_real_server_misc_check_spec.rb
@@ -0,0 +1,29 @@
+
+require 'spec_helper'
+
+describe 'keepalived::lvs::real_server::misc_check', :type => 'define' do
+  let(:title) { 'test' }
+  let(:facts) { debian_facts }
+  let(:pre_condition) { '$concat_basedir = "/tmp"' }
+
+  context 'with bare minimum: real_server, misc_path' do
+    let(:params) {
+      {
+        :virtual_server => 'virt1',
+        :real_server => 'real1',
+        :misc_path => '/bin/true'
+      }
+    }
+
+    it {
+      should contain_concat__fragment('keepalived.conf_lvs_real_server_misc_check_test').with( {
+        'content' => <<-CONTENT.gsub(/ {10}/, '  ')
+            MISC_CHECK {
+              misc_path "/bin/true"
+            }
+        CONTENT
+      })
+    }
+  end
+
+end
