62f694dae72b34a6afb6095514d60be7a1b438c0
Sergey Ivanov <sergey57@gmail.com>
2018-10-19 12:59:03 -0400

    Support SNMP for keepalived.

--- manifests/global_defs.pp
+++ manifests/global_defs.pp
@@ -23,6 +23,27 @@
 # $enable_script_security::   Set the enable_script_security option.
 #                             Default: undef.
 #
+# $snmp_socket::              Define snmp master agent socker
+#                             Default: unix:/var/agentx/master
+#
+# $enable_snmp_keepalived::   Set enable_snmp_keepalived option.
+#                             Default: undef.
+#
+# $enable_snmp_checker::      Set enable_snmp_keepalived option.
+#                             Default: undef.
+#
+# $enable_snmp_rfc::          Set enable_snmp_keepalived option.
+#                             Default: undef.
+#
+# $enable_snmp_rfcv2::        Set enable_snmp_keepalived option.
+#                             Default: undef.
+#
+# $enable_snmp_rfcv3::        Set enable_snmp_keepalived option.
+#                             Default: undef.
+#
+# $enable_traps::             Set enable_snmp_keepalived option.
+#                             Default: undef.
+#
 class keepalived::global_defs(
   $notification_email      = undef,
   $notification_email_from = undef,
@@ -31,6 +52,13 @@ class keepalived::global_defs(
   $router_id               = undef,
   $script_user             = undef,
   $enable_script_security  = undef,
+  $enable_snmp_keepalived  = undef,
+  $enable_snmp_checker     = undef,
+  $enable_snmp_rfc         = undef,
+  $enable_snmp_rfcv2       = undef,
+  $enable_snmp_rfcv3       = undef,
+  $enable_traps            = undef,
+  $snmp_socket             = 'unix:/var/agentx/master',
 ) inherits keepalived::params {
   concat::fragment { 'keepalived.conf_globaldefs':
     target  => "${::keepalived::params::config_dir}/keepalived.conf",
