7fd7d756bdfe93269995c3fbbc96ae8c57af761f
Alexander Fisher <alex@linfratech.co.uk>
2019-03-12 19:55:08 +0000

    Rubocop Autofix: Style/StringLiterals

--- spec/unit/facter/keepalived_version_spec.rb
+++ spec/unit/facter/keepalived_version_spec.rb
@@ -1,4 +1,4 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe Facter::Util::Fact do
   before {
@@ -6,17 +6,17 @@ describe Facter::Util::Fact do
     allow(Facter::Util::Resolution).to receive(:exec).with(anything)
   }
 
-  describe "keepalived_version" do
+  describe 'keepalived_version' do
     context 'returns keepalived version when keepalived present' do
       it do
         keepalived_version_output = <<-EOS
 Keepalived v1.2.2 (10/03,2013)
         EOS
-        allow(Facter::Util::Resolution).to receive(:which).with("keepalived").
+        allow(Facter::Util::Resolution).to receive(:which).with('keepalived').
           and_return(true)
-        allow(Facter::Util::Resolution).to receive(:exec).with("keepalived --version 2>&1").
+        allow(Facter::Util::Resolution).to receive(:exec).with('keepalived --version 2>&1').
           and_return(keepalived_version_output)
-        Facter.fact(:keepalived_version).value.should == "1.2.2"
+        Facter.fact(:keepalived_version).value.should == '1.2.2'
       end
     end
 
@@ -25,18 +25,18 @@ Keepalived v1.2.2 (10/03,2013)
         keepalived_version_output = <<-EOS
 Keepalived v1.2.13 (08/07,2014)
         EOS
-        allow(Facter::Util::Resolution).to receive(:which).with("keepalived").
+        allow(Facter::Util::Resolution).to receive(:which).with('keepalived').
           and_return(true)
-        allow(Facter::Util::Resolution).to receive(:exec).with("keepalived --version 2>&1").
+        allow(Facter::Util::Resolution).to receive(:exec).with('keepalived --version 2>&1').
           and_return(keepalived_version_output)
-        Facter.fact(:keepalived_version).value.should == "1.2.13"
+        Facter.fact(:keepalived_version).value.should == '1.2.13'
       end
     end
 
     context 'returns nil when keepalived not present' do
       it do
         allow(Facter::Util::Resolution).to receive(:exec)
-        allow(Facter::Util::Resolution).to receive(:which).with("keepalived").
+        allow(Facter::Util::Resolution).to receive(:which).with('keepalived').
           and_return(false)
         Facter.fact(:keepalived_version).should be_nil
       end
