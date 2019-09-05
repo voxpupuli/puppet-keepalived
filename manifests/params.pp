49132d3248cb3c4450c3d81a7ecaf2b2ee1ff394
Adrian Hungate <adrian@limbicly.com>
2019-09-05 19:47:13 +0100

    Add compatibility for Alpine Linux

--- manifests/params.pp
+++ manifests/params.pp
@@ -57,6 +57,22 @@ class keepalived::params {
       $service_name       = 'keepalived'
     }
 
+    'Linux': {
+      $sysconf_dir        = 'conf.d'
+      $sysconf_options    = ''
+      $config_dir         = '/etc/keepalived'
+      $config_dir_mode    = '0755'
+      $config_file_mode   = '0644'
+      $config_group       = 'root'
+      $config_owner       = 'root'
+      $daemon_group       = 'root'
+      $daemon_user        = 'root'
+      $pkg_list           = [ 'keepalived' ]
+      $service_hasrestart = false
+      $service_hasstatus  = false
+      $service_name       = 'keepalived'
+    }
+
     default: {
       fail "Operating system ${facts['operatingsystem']} is not supported."
     }
