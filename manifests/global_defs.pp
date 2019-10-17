7594c3d4074dbb671fdf9659f43652ae96fbb27b
Stefan Schwarz <ssz@bitsbeats.com>
2019-10-17 17:23:41 +0200

    add vrrp_higher_prio_send_advert + vrrp_garp_lower_prio_repeat opts

--- manifests/global_defs.pp
+++ manifests/global_defs.pp
@@ -2,63 +2,71 @@
 #
 # === Parameters:
 #
-# $notification_email::       Array of notification email Recipients.
-#                             Default: undef.
+# $notification_email::            Array of notification email Recipients.
+#                                  Default: undef.
 #
-# $notification_email_from::  Define the notification email Sender.
-#                             Default: undef.
+# $notification_email_from::       Define the notification email Sender.
+#                                  Default: undef.
 #
-# $smtp_server::              Define the smtp server addres.
-#                             Default: undef.
+# $smtp_server::                   Define the smtp server addres.
+#                                  Default: undef.
 #
-# $smtp_connect_timeout::     Define the smtp connect timeout.
-#                             Default: undef.
+# $smtp_connect_timeout::          Define the smtp connect timeout.
+#                                  Default: undef.
 #
-# $router_id::                Define the router ID.
-#                             Default: undef.
+# $router_id::                     Define the router ID.
+#                                  Default: undef.
 #
-# $script_user                Set the global script_user option.
-#                             Default: undef.
+# $script_user                     Set the global script_user option.
+#                                  Default: undef.
 #
-# $enable_script_security::   Set the enable_script_security option.
-#                             Default: undef.
+# $enable_script_security::        Set the enable_script_security option.
+#                                  Default: undef.
 #
-# $snmp_socket::              Define snmp master agent socker
-#                             Default: unix:/var/agentx/master
+# $snmp_socket::                   Define snmp master agent socker
+#                                  Default: unix:/var/agentx/master
 #
-# $enable_snmp_keepalived::   Set enable_snmp_keepalived option.
-#                             Default: undef.
+# $enable_snmp_keepalived::        Set enable_snmp_keepalived option.
+#                                  Default: undef.
 #
-# $enable_snmp_checker::      Set enable_snmp_keepalived option.
-#                             Default: undef.
+# $enable_snmp_checker::           Set enable_snmp_keepalived option.
+#                                  Default: undef.
 #
-# $enable_snmp_rfc::          Set enable_snmp_keepalived option.
-#                             Default: undef.
+# $enable_snmp_rfc::               Set enable_snmp_keepalived option.
+#                                  Default: undef.
 #
-# $enable_snmp_rfcv2::        Set enable_snmp_keepalived option.
-#                             Default: undef.
+# $enable_snmp_rfcv2::             Set enable_snmp_keepalived option.
+#                                  Default: undef.
 #
-# $enable_snmp_rfcv3::        Set enable_snmp_keepalived option.
-#                             Default: undef.
+# $enable_snmp_rfcv3::             Set enable_snmp_keepalived option.
+#                                  Default: undef.
 #
-# $enable_traps::             Set enable_snmp_keepalived option.
-#                             Default: undef.
+# $enable_traps::                  Set enable_snmp_keepalived option.
+#                                  Default: undef.
+#
+# $vrrp_higher_prio_send_advert::  Set vrrp_higher_prio_send_advert option.
+#                                  Default: undef.
+#
+# $vrrp_garp_lower_prio_repeat::  Set vrrp_garp_lower_prio_repeat option.
+#                                  Default: undef.
 #
 class keepalived::global_defs(
-  $notification_email      = undef,
-  $notification_email_from = undef,
-  $smtp_server             = undef,
-  $smtp_connect_timeout    = undef,
-  $router_id               = undef,
-  $script_user             = undef,
-  $enable_script_security  = undef,
-  $enable_snmp_keepalived  = undef,
-  $enable_snmp_checker     = undef,
-  $enable_snmp_rfc         = undef,
-  $enable_snmp_rfcv2       = undef,
-  $enable_snmp_rfcv3       = undef,
-  $enable_traps            = undef,
-  $snmp_socket             = 'unix:/var/agentx/master',
+  $notification_email                             = undef,
+  $notification_email_from                        = undef,
+  $smtp_server                                    = undef,
+  $smtp_connect_timeout                           = undef,
+  $router_id                                      = undef,
+  $script_user                                    = undef,
+  $enable_script_security                         = undef,
+  $enable_snmp_keepalived                         = undef,
+  $enable_snmp_checker                            = undef,
+  $enable_snmp_rfc                                = undef,
+  $enable_snmp_rfcv2                              = undef,
+  $enable_snmp_rfcv3                              = undef,
+  $enable_traps                                   = undef,
+  Optional[Boolean] $vrrp_higher_prio_send_advert = undef,
+  Optional[Integer] $vrrp_garp_lower_prio_repeat  = undef,
+  $snmp_socket                                    = 'unix:/var/agentx/master',
 ) inherits keepalived::params {
   concat::fragment { 'keepalived.conf_globaldefs':
     target  => "${keepalived::params::config_dir}/keepalived.conf",
--- spec/classes/keepalived_global_defs_spec.rb
+++ spec/classes/keepalived_global_defs_spec.rb
@@ -220,6 +220,35 @@ describe 'keepalived::global_defs', type: :class do
             )
         }
       end
+      describe 'with vrrp_higher_prio_send_advert' do
+        let(:params) do
+          {
+            vrrp_higher_prio_send_advert: true
+          }
+        end
+
+        it {
+          is_expected.to \
+            contain_concat__fragment('keepalived.conf_globaldefs').with(
+              'content' => %r{vrrp_higher_prio_send_advert true$}
+            )
+        }
+      end
+
+      describe 'with parameter vrrp_garp_lower_prio_repeat' do
+        let(:params) do
+          {
+            vrrp_garp_lower_prio_repeat: 3
+          }
+        end
+
+        it {
+          is_expected.to \
+            contain_concat__fragment('keepalived.conf_globaldefs').with(
+              'content' => %r{vrrp_garp_lower_prio_repeat 3$}
+            )
+        }
+      end
 
       describe 'with parameter snmp_socket' do
         let(:params) do
