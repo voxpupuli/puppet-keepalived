5ff50f4f25cb4030b8febc3ad8168000b678793b
Jan Vanmullem <jan.vanmullem@infrabel.be>
2022-04-25 16:57:02 +0200

    Add support for dynamic_interfaces in global_defs section

--- manifests/global_defs.pp
+++ manifests/global_defs.pp
@@ -77,6 +77,8 @@
 #
 # @param max_auto_priority Set max_auto_priority option.
 #
+# @param dynamic_interfaces Set the dynamic_interfaces option.
+#
 class keepalived::global_defs (
   $notification_email                                = undef,
   $notification_email_from                           = undef,
@@ -115,6 +117,7 @@ class keepalived::global_defs (
   Boolean $vrrp_no_swap                              = false,
   Optional[Integer[2, 3]] $vrrp_version              = undef,
   Optional[Integer[-1, 99]] $max_auto_priority       = undef,
+  Boolean $dynamic_interfaces                        = false,
   $snmp_socket                                       = 'unix:/var/agentx/master',
 ) {
   concat::fragment { 'keepalived.conf_globaldefs':
